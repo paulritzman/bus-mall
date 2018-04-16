e7547a89595a09c770d762d57377f8ca8f09ee54
paulritzman <ritzman.paul@gmail.com>
2018-04-16 15:59:43 -0700

    Added code comments for understandability; Modified event handler function and general code layout

--- js/app.js
+++ js/app.js
@@ -1,12 +1,15 @@
 'use strict';
 
+// Declares instance variables
 var votingRounds = 25;
-var imgLeft, imgCenter, imgRight;
-var arrCatalogItems = [
+var imgLeft, imgCenter, imgRight; // Declares variables used to store arrCatalogItems indexes
+var arrCatalogItems = [ // Array to create and store CatalogItem instances
   new CatalogItem('testImg1', 'img/bag.jpg'),
   new CatalogItem ('testImg2', 'img/banana.jpg'),
   new CatalogItem('testImg3', 'img/bathroom.jpg')
 ];
+
+// Declares and instantiates event listeners and related event handlers
 var leftCatalogImage = document.getElementById('left-catalog-image');
 var centerCatalogImage = document.getElementById('center-catalog-image');
 var rightCatalogImage = document.getElementById('right-catalog-image');
@@ -15,6 +18,8 @@ leftCatalogImage.addEventListener('click', handleUserVote);
 centerCatalogImage.addEventListener('click', handleUserVote);
 rightCatalogImage.addEventListener('click', handleUserVote);
 
+// Declares CatalogItem constructor - takes in image name and image url
+// Sets appearances and votes to zero - incrementing upon image appearing on page or user click event, respectively
 function CatalogItem(name, url) {
   this.name = name;
   this.url = url;
@@ -22,6 +27,9 @@ function CatalogItem(name, url) {
   this.votes = 0;
 }
 
+// Event handler for user click events
+// Increments CatalogItem vote count and executes function to display 3 new images
+// Decrements number of rounds left for user to vote on images
 function handleUserVote(event) {
   event.preventDefault();
 
@@ -35,8 +43,12 @@ function handleUserVote(event) {
   } else {
     imgRight.votes++;
   }
+
+  votingRounds--;
 }
 
+// Selects and displays 3 new images at random (Ignoring previous 3 images shown)
+// Executes on page load and upon user click events
 function pickNewCatalogItems() {
   imgLeft = arrCatalogItems[Math.floor(Math.random() * arrCatalogItems.length)];
   leftCatalogImage.src = imgLeft.url;
