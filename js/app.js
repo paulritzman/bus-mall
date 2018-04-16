09c84dbba4ee8771a8a6255d3f560cc8cbb3badf
paulritzman <ritzman.paul@gmail.com>
2018-04-16 15:21:07 -0700

    Enabled JS functionality to link images to index.html and randomly select image order; linked app.js to index.html

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@
 
 var votingRounds = 25;
 
-var arrCatalogItems = [];
+var arrCatalogItems = [new CatalogItem('testImg1', 'img/bag.jpg'), new CatalogItem ('testImg2', 'img/banana.jpg'), new CatalogItem('testImg3', 'img/bathroom.jpg')];
 
 function CatalogItem(name, url) {
   this.name = name;
@@ -10,3 +10,20 @@ function CatalogItem(name, url) {
   this.appearances = 0;
   this.votes = 0;
 }
+
+var leftCatalogImage = document.getElementById('left-catalog-image');
+var centerCatalogImage = document.getElementById('center-catalog-image');
+var rightCatalogImage = document.getElementById('right-catalog-image');
+
+function pickNewCatalogItems() {
+  var imgLeft = arrCatalogItems[Math.floor(Math.random() * arrCatalogItems.length)];
+  leftCatalogImage.src = imgLeft.url;
+
+  var imgCenter = arrCatalogItems[Math.floor(Math.random() * arrCatalogItems.length)];
+  centerCatalogImage.src = imgCenter.url;
+
+  var imgRight = arrCatalogItems[Math.floor(Math.random() * arrCatalogItems.length)];
+  rightCatalogImage.src = imgRight.url;
+}
+
+pickNewCatalogItems();
