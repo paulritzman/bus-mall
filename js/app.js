0361166dfc08c1ef6eff016ef4eb83fa7cebb339
paulritzman <ritzman.paul@gmail.com>
2018-04-16 15:31:21 -0700

    Created event handler for user clicking on any of the catalog images

--- js/app.js
+++ js/app.js
@@ -15,15 +15,29 @@ var leftCatalogImage = document.getElementById('left-catalog-image');
 var centerCatalogImage = document.getElementById('center-catalog-image');
 var rightCatalogImage = document.getElementById('right-catalog-image');
 
+leftCatalogImage.addEventListener('click', handleUserVote);
+centerCatalogImage.addEventListener('click', handleUserVote);
+rightCatalogImage.addEventListener('click', handleUserVote);
+
+function handleUserVote(event) {
+  event.preventDefault();
+
+  var userSelection = event.target;
+  console.log('The user clicked ' + userSelection.id);
+}
+
 function pickNewCatalogItems() {
   var imgLeft = arrCatalogItems[Math.floor(Math.random() * arrCatalogItems.length)];
   leftCatalogImage.src = imgLeft.url;
+  imgLeft.appearances++;
 
   var imgCenter = arrCatalogItems[Math.floor(Math.random() * arrCatalogItems.length)];
   centerCatalogImage.src = imgCenter.url;
+  imgCenter.appearances++;
 
   var imgRight = arrCatalogItems[Math.floor(Math.random() * arrCatalogItems.length)];
   rightCatalogImage.src = imgRight.url;
+  imgRight.appearances++;
 }
 
 pickNewCatalogItems();
