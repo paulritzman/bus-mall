1636d1a657eb37ccd92bde547925586434dd8669
paulritzman <ritzman.paul@gmail.com>
2018-04-16 15:44:48 -0700

    Improved event handler to increment number of votes upon click event

--- js/app.js
+++ js/app.js
@@ -1,16 +1,12 @@
 'use strict';
 
 var votingRounds = 25;
-
-var arrCatalogItems = [new CatalogItem('testImg1', 'img/bag.jpg'), new CatalogItem ('testImg2', 'img/banana.jpg'), new CatalogItem('testImg3', 'img/bathroom.jpg')];
-
-function CatalogItem(name, url) {
-  this.name = name;
-  this.url = url;
-  this.appearances = 0;
-  this.votes = 0;
-}
-
+var imgLeft, imgCenter, imgRight;
+var arrCatalogItems = [
+  new CatalogItem('testImg1', 'img/bag.jpg'),
+  new CatalogItem ('testImg2', 'img/banana.jpg'),
+  new CatalogItem('testImg3', 'img/bathroom.jpg')
+];
 var leftCatalogImage = document.getElementById('left-catalog-image');
 var centerCatalogImage = document.getElementById('center-catalog-image');
 var rightCatalogImage = document.getElementById('right-catalog-image');
@@ -19,23 +15,38 @@ leftCatalogImage.addEventListener('click', handleUserVote);
 centerCatalogImage.addEventListener('click', handleUserVote);
 rightCatalogImage.addEventListener('click', handleUserVote);
 
+function CatalogItem(name, url) {
+  this.name = name;
+  this.url = url;
+  this.appearances = 0;
+  this.votes = 0;
+}
+
 function handleUserVote(event) {
   event.preventDefault();
 
   var userSelection = event.target;
   console.log('The user clicked ' + userSelection.id);
+
+  if (userSelection.id === 'left-catalog-image') {
+    imgLeft.votes++;
+  } else if (userSelection.id === 'center-catalog-image') {
+    imgCenter.votes++;
+  } else {
+    imgRight.votes++;
+  }
 }
 
 function pickNewCatalogItems() {
-  var imgLeft = arrCatalogItems[Math.floor(Math.random() * arrCatalogItems.length)];
+  imgLeft = arrCatalogItems[Math.floor(Math.random() * arrCatalogItems.length)];
   leftCatalogImage.src = imgLeft.url;
   imgLeft.appearances++;
 
-  var imgCenter = arrCatalogItems[Math.floor(Math.random() * arrCatalogItems.length)];
+  imgCenter = arrCatalogItems[Math.floor(Math.random() * arrCatalogItems.length)];
   centerCatalogImage.src = imgCenter.url;
   imgCenter.appearances++;
 
-  var imgRight = arrCatalogItems[Math.floor(Math.random() * arrCatalogItems.length)];
+  imgRight = arrCatalogItems[Math.floor(Math.random() * arrCatalogItems.length)];
   rightCatalogImage.src = imgRight.url;
   imgRight.appearances++;
 }
