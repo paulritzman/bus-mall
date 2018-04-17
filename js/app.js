2e35611c6a3b46ea63104a8df3c554d1cd798e4f
paulritzman <ritzman.paul@gmail.com>
2018-04-16 17:39:01 -0700

    Completed header styling for index.html; Added alt text property for constructor function

--- js/app.js
+++ js/app.js
@@ -14,26 +14,26 @@ var imgLeft, imgCenter, imgRight; // Declares variables used to store arrCatalog
 
 // Array to create and store CatalogItem instances
 var arrCatalogItems = [
-  new CatalogItem('R2-D2 Suitcase', 'img/bag.jpg'),
-  new CatalogItem ('Banana Cutter', 'img/banana.jpg'),
-  new CatalogItem('Tablet/TP Holder', 'img/bathroom.jpg'),
-  new CatalogItem('Rain Boots', 'img/boots.jpg'),
-  new CatalogItem('All-in-One Breakfast Appliance', 'img/breakfast.jpg'),
-  new CatalogItem('Meatball Bubblegum', 'img/bubblegum.jpg'),
-  new CatalogItem('Chair', 'img/chair.jpg'),
-  new CatalogItem('Cthulhu Figurine', 'img/cthulhu.jpg'),
-  new CatalogItem('Duck Muzzle', 'img/dog-duck.jpg'),
-  new CatalogItem('Dragon Meat', 'img/dragon.jpg'),
-  new CatalogItem('Utensil Pen Caps', 'img/pen.jpg'),
-  new CatalogItem('Pet Dusters', 'img/pet-sweep.jpg'),
-  new CatalogItem('Pizza Scissors', 'img/scissors.jpg'),
-  new CatalogItem('Shark Sleeping Bag', 'img/shark.jpg'),
-  new CatalogItem('Sweeping Onesie', 'img/sweep.png'),
-  new CatalogItem('TaunTaun Sleeping Bag', 'img/tauntaun.jpg'),
-  new CatalogItem('Unicorn Meat', 'img/unicorn.jpg'),
-  new CatalogItem('Octo-Leg USB', 'img/usb.gif'),
-  new CatalogItem('Watering Can', 'img/water-can.jpg'),
-  new CatalogItem('Wine Glass', 'img/wine-glass.jpg')
+  new CatalogItem('R2-D2 Suitcase', 'img/bag.jpg', 'R2-D2 designed rolling suit case'),
+  new CatalogItem('Banana Cutter', 'img/banana.jpg', 'Plastic banana slicer'),
+  new CatalogItem('Tablet/TP Holder', 'img/bathroom.jpg', 'Dual table and toilet paper holder'),
+  new CatalogItem('Rain Boots', 'img/boots.jpg', 'Rain boots with shoe top cut-out'),
+  new CatalogItem('All-in-One Breakfast Appliance', 'img/breakfast.jpg', 'Multi-use appliance for cooking'),
+  new CatalogItem('Meatball Bubblegum', 'img/bubblegum.jpg', 'Bubblegum shaped like meatballs'),
+  new CatalogItem('Chair', 'img/chair.jpg', 'Chair with dome shaped seat'),
+  new CatalogItem('Cthulhu Figurine', 'img/cthulhu.jpg', 'Cthulhu figuring with accompanying toy soldier'),
+  new CatalogItem('Duck Muzzle', 'img/dog-duck.jpg', 'Duck-bill shaped dog muzzle'),
+  new CatalogItem('Dragon Meat', 'img/dragon.jpg', 'Canned dragon meat'),
+  new CatalogItem('Utensil Pen Caps', 'img/pen.jpg', 'Pen caps shaped like eating utensils'),
+  new CatalogItem('Pet Dusters', 'img/pet-sweep.jpg', 'Duster booties for dogs'),
+  new CatalogItem('Pizza Scissors', 'img/scissors.jpg', 'Dual scissors and spatula for pizza'),
+  new CatalogItem('Shark Sleeping Bag', 'img/shark.jpg', 'Shark shaped sleeping bag'),
+  new CatalogItem('Sweeping Onesie', 'img/sweep.png', 'Baby onesie with duster on front'),
+  new CatalogItem('TaunTaun Sleeping Bag', 'img/tauntaun.jpg', 'TaunTaun design sleeping bag'),
+  new CatalogItem('Unicorn Meat', 'img/unicorn.jpg', 'Canned unicorn meat'),
+  new CatalogItem('Octo-Leg USB', 'img/usb.gif', 'Moving octopus tentacle USB dongle'),
+  new CatalogItem('Watering Can', 'img/water-can.jpg', 'Curved watering can'),
+  new CatalogItem('Wine Glass', 'img/wine-glass.jpg', 'Wine glass')
 ];
 
 // Declares and instantiates event listeners and related event handlers
@@ -45,11 +45,12 @@ leftCatalogImage.addEventListener('click', handleUserVote);
 centerCatalogImage.addEventListener('click', handleUserVote);
 rightCatalogImage.addEventListener('click', handleUserVote);
 
-// Declares CatalogItem constructor - takes in image name and image url
+// Declares CatalogItem constructor - takes in image name, image url, and image alt text
 // Sets appearances and votes to zero - incrementing upon image appearing on page or user click event, respectively
-function CatalogItem(name, url) {
+function CatalogItem(name, url, altText) {
   this.name = name;
   this.url = url;
+  this.altText = altText;
   this.appearances = 0;
   this.votes = 0;
 }
@@ -80,14 +81,20 @@ function handleUserVote(event) {
 function pickNewCatalogItems() {
   imgLeft = arrCatalogItems[Math.floor(Math.random() * arrCatalogItems.length)];
   leftCatalogImage.src = imgLeft.url;
+  leftCatalogImage.altText = imgLeft.altText;
+
   imgLeft.appearances++;
 
   imgCenter = arrCatalogItems[Math.floor(Math.random() * arrCatalogItems.length)];
   centerCatalogImage.src = imgCenter.url;
+  centerCatalogImage.altText = imgCenter.altText;
+
   imgCenter.appearances++;
 
   imgRight = arrCatalogItems[Math.floor(Math.random() * arrCatalogItems.length)];
   rightCatalogImage.src = imgRight.url;
+  rightCatalogImage.altText = imgRight.altText;
+
   imgRight.appearances++;
 }
 
